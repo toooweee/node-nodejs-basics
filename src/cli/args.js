222a675665ebc8685abe6db543f5d6ff7a33a8bf
toooweee <byprob03@gmail.com>
2025-10-26 01:11:10 +0500

    feat: (args) done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
 const parseArgs = () => {
   // Write your code here
+  const args = process.argv.slice(2);
+  let result = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+
+    if (arg.startsWith("--")) {
+      result.push(`${arg} is ${args[i + 1]}`);
+    }
+  }
+
+  result = result.join(", ");
+
+  console.log(result);
 };
 
 parseArgs();
